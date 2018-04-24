db77efd5e70ccea3594e422985a7cdfdfb6b4f55
rafael carvalho monteiro <1727953>
2018-04-24 17:11:55 -0300

    ajustes

--- basicOperations.c
+++ basicOperations.c
@@ -1,4 +1,11 @@
 #include "basicOperations.h";
+typedef struct Multiplicao {
+    int A:4;
+    int Q:4;
+    unsigned int Q_1:1;
+    int M:4;
+} multiplicacao;
+
 int complementar(int x) { return (~x) + 1; }
 
 int soma(int x, int y) {
@@ -18,7 +25,12 @@ int soma(int x, int y) {
 int subtracao(int x, int y) { return soma(x, complementar(y)); }
 
 void produto(int x, int y) {
-    //TODO
+    multiplicacao m;
+    int count = 4;
+    m.A = 0;
+    m.Q_1 = 0;
+    m.Q = x;
+    m.M = y;
 }
 
 void divisao(int x, int y) {
@@ -40,6 +52,7 @@ void int2bin(int n){
 
 //PRIVATE FUNCTIONS
 int __extractLastBit(int x) { return x & 1; }
+int __extractFirstBit(int x) { return x & 8; }
 int __rightShift(int x) { return x >> 1; }
 int __leftShift(int x) { return x << 1; }
 
